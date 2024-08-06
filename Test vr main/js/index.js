e87ffb193a525c18d3ca5b1252e99572a5e3dc3f
Fedkiv Oleh <148952611+Pibepi@users.noreply.github.com>
2024-08-06 11:47:37 +0300

    Add files via upload

---
+++ Test vr main/js/index.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Ініціалізація Swiper
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            1268: {
+                slidesPerView: 3,
+                spaceBetween: 10
+            }
+        }
+    });
+
+    // Ініціалізація усіх компонентів Materialize
+    M.AutoInit();
+
+    // Ініціалізація каруселі
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        fullWidth: true,
+        indicators: true
+    });
+
+    // Додавання класу активності кнопкам
+    document.querySelectorAll('.btnTours').forEach(button => {
+        button.addEventListener('click', function () {
+            document.querySelectorAll('.btnTours').forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
